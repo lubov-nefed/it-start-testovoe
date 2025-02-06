c4ebfeb15c3a20e8006170d71fbce79949c4ae79
lubov-nefed <love.nefed@gmail.com>
2025-02-06 13:47:04 +0300

    feat: add seminars fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
+
+async function fetchSeminars() {
+  try {
+    const response = await fetch("/db.json");
+    if (response.ok) {
+      let data = await response.json();
+      return data.seminars;
+    } else {
+      throw Error(
+        `Something went wrong. Response status code: ${response.status}`
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function App() {
+  const [seminars, setSeminars] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    setSeminars(null);
+    fetchSeminars().then((result) => {
+      if (!ignore) {
+        setSeminars(result);
+      }
+    });
+    return () => {
+      ignore = true;
+    };
+  }, []);
+  console.log(seminars);
+
   return <>App</>;
 }
 
