5445635fd3595a42fdce9dce5edef33bd09f3d31
lubov-nefed <love.nefed@gmail.com>
2025-02-07 18:33:35 +0300

    feat: add EditForm

--- src/ModalWindow.jsx
+++ src/ModalWindow.jsx
@@ -1,8 +1,31 @@
+function EditForm({ seminar }) {
+  const keys = Object.keys(seminar).map((item) => {
+    if (item !== "id") {
+      return item;
+    }
+  });
+  console.log(keys);
+  const fields = keys.map((key) => (
+    <label key={key} htmlFor={key}>
+      <input
+        key={key}
+        type="text"
+        name={key}
+        id={key}
+        defaultValue={seminar[key]}
+      />
+    </label>
+  ));
+  return <form>{fields}</form>;
+}
+
 export function ModalWindow({ type, activeSeminar, onClose }) {
+  console.log(activeSeminar);
   return (
     <div className="modal-window">
       ModalWindow type: {type}
       <br />
+      {type === "editModal" && <EditForm seminar={activeSeminar} />}
       Active Seminar: {activeSeminar.title}
       <button onClick={onClose}>Close</button>
     </div>
